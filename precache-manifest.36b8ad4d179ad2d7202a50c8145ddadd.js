30005d694327d11bdfe835c758adc4940d3639e2
Ryan Thomas <rthomas7@uw.edu>
2019-03-16 00:22:33 -0700

    First version of sightreader

---
+++ precache-manifest.36b8ad4d179ad2d7202a50c8145ddadd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "326dd4d4f56c4720319f",
+    "url": "/static/js/main.2b43af02.chunk.js"
+  },
+  {
+    "revision": "3fbc696ca30ad7532b94",
+    "url": "/static/js/2.8d3a96f7.chunk.js"
+  },
+  {
+    "revision": "326dd4d4f56c4720319f",
+    "url": "/static/css/main.0d147cf2.chunk.css"
+  },
+  {
+    "revision": "3fbc696ca30ad7532b94",
+    "url": "/static/css/2.39e8449d.chunk.css"
+  },
+  {
+    "revision": "e58bb46ddb52f94ba9d1298996ede13a",
+    "url": "/index.html"
+  }
+];
